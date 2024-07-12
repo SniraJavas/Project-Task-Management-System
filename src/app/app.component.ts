c0feac37c352bf11c6be062da1cb567a09b51f00
Sinikiwe Nangoku Jumba <snirajumba@gmail.com>
2024-07-12 21:09:59 +0200

    Home and Tasks components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+
+    },
+    {
+        path:'tasks',
+        component: TaskListComponent
+    }
+];
